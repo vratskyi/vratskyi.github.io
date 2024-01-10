4623d6063c866eb76466d122f8cec29c57d9106c
Oleksii Vratskyi <lesha0skorohod@gmail.com>
2024-01-10 16:33:47 +0200

    Optimization, mobile improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://vratskyi.github.io'
+  site: 'https://vratskyi.github.io',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  }
 });
