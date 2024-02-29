ff05601bb6b4109018272e7cffe04d1f60bdc709
vratskyi <lesha0skorohod@gmail.com>
2024-02-29 18:00:21 +0200

    Configuring the vercel adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,24 @@ import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // i18n settings
-i18n: {
-  defaultLocale: "en",
-  locales: ["en", "ru", {
-    path: "ru",
-    codes: ["ru", "ru-RU"]
-}],
-  routing: {
-    prefixDefaultLocale: true
-},
-  fallback: {
-    ru: "en"
-}
-},
-  output: "hybrid",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru", {
+      path: "ru",
+      codes: ["ru", "ru-RU"]
+    }],
+    routing: {
+      prefixDefaultLocale: true
+    },
+    fallback: {
+      ru: "en"
+    }
+  },
   integrations: [mdx(), compress({
     CSS: true,
     HTML: false,
@@ -27,9 +28,18 @@ i18n: {
     JavaScript: true,
     SVG: false
   }), tailwind()],
-  site: 'http://localhost:4321',
+  // site: 'http://localhost:4321',
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport'
-  }
+  },
+  output: "hybrid",
+  adapter: vercel({
+    isr: {
+      expiration: 60 * 60 * 24,
+    },
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
